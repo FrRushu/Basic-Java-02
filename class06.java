974b35b29bffde1c64ade7b5cecb8bf9ddbce5ac
FrRushu <frushu@gmail.com>
2017-05-16 22:36:01 +0600

    03. Keyword And Expression

---
+++ class06.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Rushu on 5/15/2017.
+ * Keyword And Expression
+ */
+public class class06 {
+    public static void main(String args[]) {
+        //http://javaconceptoftheday.com/java-keywords-with-examples/
+        //given link is for reserve keyword in java  (50 key word)
+
+
+        // 1 mile  = 1.609344 kilometers
+
+        double kilometers = (100 * 1.609344);
+
+        System.out.println(kilometers);
+
+        int higScore = 98;
+        if (higScore == 98) {
+            System.out.println("this is expression ");
+        }
+
+    }
+}
